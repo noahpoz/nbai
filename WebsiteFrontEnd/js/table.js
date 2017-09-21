e1e76ca91fcf6d6afff2b6b23cd8da099b80fb30
Jeffreywye <jeffreywye7@gmail.com>
2017-09-21 18:03:20 -0400

    Added  buttons to index.html Search Bar and Table CSS still incomplete.

--- WebsiteFrontEnd/js/table.js
+++ WebsiteFrontEnd/js/table.js
@@ -1,1 +1,11 @@
-var searchContent = document.getElementById("");
+// var searchContent = document.getElementById("");
+
+var $rows = $('#table tr');
+$('#search').keyup(function() {
+    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+    
+    $rows.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(val);
+    }).hide();
+});
